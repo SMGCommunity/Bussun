c46d7781b11669d496e2ed084af8adab93ad3030
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2024-03-30 22:24:51 -0500

    add kamek.h

---
+++ include/kamek.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Bussun.h"
+#include "Petari/libs/RVL_SDK/include/revolution/types.h"
+
+struct KamekHeader
+{
+    u32 magic1;
+    u16 magic2;
+    u16 version;
+    u32 bssSize;
+    u32 codeSize;
+
+    size_t HeaderSize() const {
+        if (version == 2)
+            return 32;
+        else if (version == 1)
+            return 16;
+        else
+            return (size_t)-1;
+    }
+};
