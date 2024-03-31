563addfaaa9b555cf04371d6f497b1e5cb937de3
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2024-03-30 22:23:45 -0500

    update setup links

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import urllib.request
 import io
 from zipfile import ZipFile
 
-kamek = "https://cdn.discordapp.com/attachments/1119750120657911849/1145047376936050730/Kamek.zip"
+kamek = "https://mariogalaxy.org/Kamek-v1.zip"
 cw = "http://shibbo.net/smg/Compilers.zip"
 
 def setup():
@@ -10,10 +10,7 @@ def setup():
         data = io.BytesIO(response.read())
         with ZipFile(data, "r") as zip:
             zip.extractall("Compilers")
-    headers = {}
-    headers["User-Agent"]='Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0'
-    request = urllib.request.Request(kamek, headers=headers)
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(kamek) as response:
         data = io.BytesIO(response.read())
         with ZipFile(data, "r") as zip:
             zip.extractall("Kamek")
