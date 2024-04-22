0b564542ec1cc2c232989d49e84a1313e2761ca3
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2024-04-22 09:28:21 -0500

    Change static assert tests

--- Bussun.h
+++ Bussun.h
@@ -3,13 +3,9 @@
 #include "revolution/types.h"
 template<bool> struct StaticAssert;
 template<> struct StaticAssert<true> {static const bool result = true;};
-#ifdef __COUNTER__
-#define CONCAT_IMPL( x, y ) x##y
-#define MACRO_CONCAT( x, y ) CONCAT_IMPL( x, y )
-#define STATIC_ASSERT(expr, msg) \
-typedef StaticAssert<expr> msg; \
-const bool MACRO_CONCAT(Assert, __COUNTER__) = msg::result
+const bool size_of_pointer_must_be_4 = StaticAssert<sizeof(void*) == 4>::result;
+const bool size_of_long_must_be_4 = StaticAssert<sizeof(long) == 4>::result;
+#ifndef __COUNTER__
+#error __COUNTER__ is NOT defined! This will cause issues!
 #endif
-STATIC_ASSERT(sizeof(void*) == 4, size_of_pointer_must_be_4);
-STATIC_ASSERT(sizeof(long) == 4, size_of_long_must_be_4);
 typedef unsigned long size_t;
